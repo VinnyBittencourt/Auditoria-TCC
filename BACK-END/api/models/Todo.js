eddcf20a577a66508d294e8facd8610b83256999
Frederico Cavalcanti <fredcsmsampaio@gmail.com>
2019-11-05 19:51:21 -0300

    Add files via upload

---
+++ BACK-END/api/models/Todo.js
@@ -0,0 +1,358 @@
+/**
+ * Todo.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    pegunta1:{
+        type:'string',
+        required:true,
+    },
+    pegunta2:{
+        type:'string',
+        required:true,
+    },
+    pegunta3:{
+        type:'string',
+        required:true,
+    },
+    pegunta4:{
+        type:'string',
+        required:true,
+    },
+    pegunta5:{
+        type:'string',
+        required:false,
+    },
+    pegunta6:{
+        type:'string',
+        required:false,
+    },
+    pegunta7:{
+        type:'string',
+        required:true,
+    },
+    pegunta8:{
+        type:'string',
+        required:false ,
+    },
+    pegunta9:{
+        type:'string',
+        required:false ,
+    },
+    pegunta10:{
+        type:'string',
+        required:true ,
+    },
+    pegunta11:{
+        type:'string',
+        required:false ,
+    },
+    pegunta12:{
+        type:'string',
+        required:false ,
+    },
+    pegunta13:{
+        type:'string',
+        required:true ,
+    },
+    pegunta14:{
+        type:'string',
+        required:true ,
+    },
+    pegunta15:{
+        type:'string',
+        required:true ,
+    },
+    pegunta16:{
+        type:'string',
+        required:true ,
+    },
+    pegunta17:{
+        type:'string',
+        required:false ,
+    },
+    pegunta18:{
+        type:'string',
+        required:false ,
+    },
+    pegunta19:{
+        type:'string',
+        required:false ,
+    },
+    pegunta20:{
+        type:'string',
+        required:true ,
+    },
+    pegunta21:{
+        type:'string',
+        required:true ,
+    },
+    pegunta22:{
+        type:'string',
+        required:true ,
+    },
+    pegunta23:{
+        type:'string',
+        required:true ,
+    },
+    pegunta24:{
+        type:'string',
+        required:true ,
+    },
+    pegunta25:{
+        type:'string',
+        required:true ,
+    },
+    pegunta26:{
+        type:'string',
+        required:true ,
+    },
+    pegunta27:{
+        type:'string',
+        required:true ,
+    },
+    pegunta28:{
+        type:'string',
+        required:true ,
+    },
+    pegunta29:{
+        type:'string',
+        required:true ,
+    },
+    pegunta30:{
+        type:'string',
+        required:true ,
+    },
+    pegunta31:{
+        type:'string',
+        required:true ,
+    },
+    pegunta32:{
+        type:'string',
+        required:true ,
+    },
+    pegunta33:{
+        type:'string',
+        required:true ,
+    },
+    pegunta34:{
+        type:'string',
+        required:true ,
+    },
+    pegunta35:{
+        type:'string',
+        required:true ,
+    },
+    pegunta36:{
+        type:'string',
+        required:true ,
+    },
+    pegunta37:{
+        type:'string',
+        required:true ,
+    },
+    pegunta38:{
+        type:'string',
+        required:true ,
+    },
+    pegunta39:{
+        type:'string',
+        required:true ,
+    },
+    pegunta40:{
+        type:'string',
+        required:true ,
+    },
+    pegunta41:{
+        type:'string',
+        required:true ,
+    },
+    pegunta42:{
+        type:'string',
+        required:true ,
+    },
+    pegunta43:{
+        type:'string',
+        required:true ,
+    },
+    pegunta44:{
+        type:'string',
+        required:true ,
+    },
+    pegunta45:{
+        type:'string',
+        required:true ,
+    },
+    pegunta46:{
+        type:'string',
+        required:true ,
+    },
+    pegunta47:{
+        type:'string',
+        required:true ,
+    },
+    pegunta48:{
+        type:'string',
+        required:true ,
+    },
+    pegunta49:{
+        type:'string',
+        required:true ,
+    },
+    pegunta50:{
+        type:'string',
+        required:true ,
+    },
+    pegunta51:{
+        type:'string',
+        required:true ,
+    },
+    pegunta52:{
+        type:'string',
+        required:true ,
+    },
+    pegunta53:{
+        type:'string',
+        required:true ,
+    },
+    pegunta54:{
+        type:'string',
+        required:true ,
+    },
+    pegunta55:{
+        type:'string',
+        required:true ,
+    },
+    pegunta56:{
+        type:'string',
+        required:true ,
+    },
+    pegunta57:{
+        type:'string',
+        required:true ,
+    },
+    pegunta58:{
+        type:'string',
+        required:true ,
+    },
+    pegunta59:{
+        type:'string',
+        required:true ,
+    },
+    pegunta60:{
+        type:'string',
+        required:true ,
+    },
+    pegunta61:{
+        type:'string',
+        required:true ,
+    },
+    pegunta62:{
+        type:'string',
+        required:true ,
+    },
+    pegunta63:{
+        type:'string',
+        required:true ,
+    },
+    pegunta64:{
+        type:'string',
+        required:true ,
+    },
+    pegunta65:{
+        type:'string',
+        required:true ,
+    },
+    pegunta66:{
+        type:'string',
+        required:true ,
+    },
+    pegunta67:{
+        type:'string',
+        required:true ,
+    },
+    pegunta68:{
+        type:'string',
+        required:true ,
+    },
+    pegunta69:{
+        type:'string',
+        required:true ,
+    },
+    pegunta70:{
+        type:'string',
+        required:true ,
+    },
+    pegunta71:{
+        type:'string',
+        required:true ,
+    },
+    pegunta72:{
+        type:'string',
+        required:true ,
+    },
+    pegunta73:{
+        type:'string',
+        required:true ,
+    },
+    pegunta74:{
+        type:'string',
+        required:true ,
+    },
+    pegunta75:{
+        type:'string',
+        required:true ,
+    },
+    pegunta76:{
+        type:'string',
+        required:true ,
+    },
+    pegunta77:{
+        type:'string',
+        required:true ,
+    },
+    pegunta78:{
+        type:'string',
+        required:true ,
+    },
+    pegunta79:{
+        type:'string',
+        required:true ,
+    },
+    pegunta80:{
+        type:'string',
+        required:true ,
+    },
+    pegunta81:{
+        type:'string',
+        required:true ,
+    },
+    pegunta82:{
+        type:'string',
+        required:true ,
+    },
+    pegunta83:{
+        type:'string',
+        required:true ,
+    }
+  },
+
+};
+
+
+/*
+
+Array.from(document.querySelectorAll('.row.m-l-10')).map( a => {
+	Array.from(a.querySelectorAll('.radio > input')).map( b =>{
+		 if(b.checked){
+			console.log(b)
+		}
+	})
+})
+
+*/
